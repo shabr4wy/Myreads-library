1853f4ba340b09fc12d6994cc05e2634094af0dc
Abdullah yasin <abdullahyasinforwork@gmail.com>
2022-07-24 00:40:08 +0200

    biuld search feature

--- starter/src/SearchBooks.js
+++ starter/src/SearchBooks.js
@@ -1,9 +1,22 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import Book from './Book';
+import * as bookAPI from './BooksAPI'
 
 const SearchBooks = ()=> {
 
     const [query, setquery] = useState('');
-    
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect (() => {
+        const search = async () => {
+            const res = await bookAPI.search(query,10);
+            setSearchResults(res);
+            console.log(res)
+        }
+        search();
+    }, [query])
+
+
     return (
         <div className="search-books">
           <div className="search-books-bar">
@@ -22,7 +35,18 @@ const SearchBooks = ()=> {
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {
+                searchResults && searchResults.map((book)=> (
+                    <li key={book.id}>
+                        <Book previewLink={book.imageLinks.smallThumbnail}
+                              title={book.title}
+                              authors={book.authors}     
+                        />
+                    </li>
+                ))
+            }
+          </ol>
           </div>
         </div>
     ) 
