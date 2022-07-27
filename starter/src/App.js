cbd99b5693be02780d6c409940ad7b83d6ae24b7
Abdullah yasin <abdullahyasinforwork@gmail.com>
2022-07-27 20:28:52 +0200

    remove hardcode

--- starter/src/App.js
+++ starter/src/App.js
@@ -6,35 +6,10 @@ import { useState } from "react";
 
 function App(){
 
-  const [currentlyReadingBooks, setCurrentlyReadingBooks] = useState([
-    {
-      title: 'Pro React',
-      authors: ['Harper lee'],
-      coverImage: 'http://books.google.com/books/content?id=PKpPCwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      currentShelf: 'currentlyReadingBooks',
-      id: "PKpPCwAAQBAJ"
-    }
-  ]);
-
-  const [wantsToReadBooks, setWantsToReadBooks] = useState([
-    {
-      title: "Robotics",
-      authors: ["George A. Bekey"],
-      coverImage: 'http://books.google.com/books/content?id=7n2gw9MbTMUC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      currentShelf: 'wantsToReadBooks',
-      id: "7n2gw9MbTMUC",
-    },
-  ]);
-
-  const [readBooks, setReadBokks] = useState([
-    {
-      title: "Learning Virtual Reality",
-      authors: ["Tony Parisi"],
-      coverImage: 'http://books.google.com/books/content?id=tXrPCgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      currentShelf: 'readBooks',
-      id: "tXrPCgAAQBAJ",
-    }
-  ]);
+  const [currentlyReadingBooks, setCurrentlyReadingBooks] = useState([]);
+  const [wantsToReadBooks, setWantsToReadBooks] = useState([]);
+  const [readBooks, setReadBokks] = useState([]);
+  
 
   const updateBooksLibrary = (currentShelf, selectedShelf, book) => {
 
